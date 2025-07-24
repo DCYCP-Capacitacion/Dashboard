dcdd05b3806d0b6b5e263fca70a2375460d8988c
DCYCP <dcycp.cursos@gmail.com>
2025-07-24 12:52:33 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -4,13 +4,13 @@ import streamlit as st
 st.title("Mi primer Dashboard con Streamlit")
 
 # Subtítulo
-st.subheader("Este es un ejemplo básico")
+st.subheader("Hola Carlitos")
 
 # Texto
 st.write("¡Hola! Este es tu primer dashboard conectado con GitHub.")
 
 # Entrada de usuario
-nombre = st.text_input("¿Cómo te llamás?")
+nombre = st.text_input("Te gustaria saber quien soy?")
 
 # Mostrar resultado si se escribe un nombre
 if nombre:
