52a9575fb64646ddb3e960f2ed67b83c34574612
DCYCP <dcycp.cursos@gmail.com>
2025-07-24 12:58:52 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -15,3 +15,9 @@ nombre = st.text_input("Te gustaria saber quien soy?")
 # Mostrar resultado si se escribe un nombre
 if nombre:
     st.success(f"No tan rapido!")
+
+# Texto
+st.write("Primero, quiero que pienses un numero del 1 al 10")
+
+# Entrada de usuario
+nombre = st.text_input(" ")
